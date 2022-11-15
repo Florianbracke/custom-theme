defd1f3bd008be1b40e33190aabcf27fa465b218
Florian <72805220+Florianbracke@users.noreply.github.com>
2022-11-15 16:22:44 +0100

    Update functions.php

--- wp-content/themes/custom-theme/functions.php
+++ wp-content/themes/custom-theme/functions.php
@@ -123,7 +123,20 @@ function reusable_blocks_link_wp_admin() {
     add_menu_page( 'linked_url', 'Reusable Blocks', 'read', 'edit.php?post_type=wp_block', '', 'dashicons-editor-table', 22 );
 }
 
+
+/*--------------------------------------------------------------------------------------*\
+| FUNCTION REUSABLE BLOCKS TO FRONTEND
+\*--------------------------------------------------------------------------------------*/
+function get_reusable_block( $block_id ){
 	
+	$reuse_block = get_post( $block_id ); 
+	
+	if( $reuse_block == null ) return '';
+	
+	$reuse_block_content = apply_filters( 'the_content', $reuse_block->post_content);
+	
+	return $reuse_block_content;
+}	
 
 /*--------------------------------------------------------------------------------------*\
 | ADD AND REMOVE BLOCK PATTERN CATEGORIES
