e53cd26ef2848e608e5f63470562b635073fd0cc
Florian <72805220+Florianbracke@users.noreply.github.com>
2023-02-22 15:16:27 +0100

    Add more variables to activate on mobile menu click

--- custom-theme/assets/js/navigation.js
+++ custom-theme/assets/js/navigation.js
@@ -1,11 +1,12 @@
 window.addEventListener('load', () => {
 
-    let mobileButton = document.querySelector('div.menu-toggle');
+    let mobileButton = document.querySelector('.menu-toggle');
     let mobileHeader = document.querySelector('header .menu-container-mobile');
+	let body = document.querySelector('body');
 
     mobileButton.addEventListener('click', () => {
-
+		mobileButton.classList.toggle('active-mobile-header');
         mobileHeader.classList.toggle('active-mobile-header');
-
+		body.classList.toggle('active-mobile-header');
     })
 })
