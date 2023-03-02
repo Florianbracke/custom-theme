3ab05d8320908af0e86dd3c3b89826ac1af10978
Florian <72805220+Florianbracke@users.noreply.github.com>
2023-03-02 14:03:13 +0100

    Add simple layout for 404

---
+++ custom-theme/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+	<main id="primary" class="site-main">
+		<div class='entry-content'>
+			<div class='alignwide wp-block-columns'>
+				<div class=' wp-block-column'>
+					<h1>
+						Oeps, het lijkt erop dat deze pagina niet bestaat. 
+					</h1>		
+				</div>
+			</div>
+		</div>
+</main>
+
+<?php
+get_footer();
