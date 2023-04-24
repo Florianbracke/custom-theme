99480b07cac8fb3f961bdbf564e5a874698fe002
Florian <72805220+Florianbracke@users.noreply.github.com>
2023-04-24 11:12:43 +0200

    Add current page to breadcrumbs & page title instead of ID

--- custom-theme/template-parts/content-breadcrumbs.php
+++ custom-theme/template-parts/content-breadcrumbs.php
@@ -1,24 +1,28 @@
 <ul itemscope itemtype="https://schema.org/BreadcrumbList" style='list-style:none; display:flex; gap: 10px;'> <?php
 
-		$i = 0;
-		$id = get_the_ID();
+	$i = 0;
+	$id = get_the_ID();
 
-		if (get_post_parent($id) ){	
-			$ancestors = get_post_ancestors($id);
-			$ancestors = array_reverse($ancestors);
-			foreach( $ancestors as $ancestor){ 
-				$i++; ?>
+	if ( get_post_parent($id) ) {	
+		$ancestors = get_post_ancestors( $id );
+		$ancestors = array_reverse( $ancestors );
+		foreach( $ancestors as $ancestor ){ 
+			$i++; ?>
 
-				<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" style='list-style:none; display:flex; gap: 10px;'>
-					<a itemprop="item" href="<?php echo get_permalink($ancestor); ?>">
-						<span itemprop="name"><?php echo $ancestor; ?> </span>
-					</a>
-					<meta itemprop="position" content="<?php echo $i; ?>" />
-					<?php if ( $ancestors[$i]){ ?>
-						<p> > </p>
-					<?php } ?>
-				</li> <?php 
-			}
-		}
+			<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" style='list-style:none; display:flex; gap: 10px;'>
+				<a itemprop="item" href="<?php echo get_permalink( $ancestor ); ?>">
+					<span itemprop="name"><?php echo get_the_title( $ancestor ); ?> </span>
+				</a>
+				<meta itemprop="position" content="<?php echo $i; ?>" />
+			</li> 
+			<p> > </p>
+		<?php } ?>
 
-	?> </ul> <?php
+		<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" style='list-style:none; display:flex; gap: 10px;'>
+			<a itemprop="item" href="<?php echo get_permalink( $id ); ?>">
+				<span itemprop="name"><?php echo get_the_title( $id ); ?> </span>
+			</a>
+			<meta itemprop="position" content="<?php echo count( $ancestors ); ?>" />
+		</li> <?php 
+	}
+?> </ul> <?php
