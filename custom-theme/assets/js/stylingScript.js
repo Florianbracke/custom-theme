f3929dc767d80151131355ad39237676729a933b
Florian <72805220+Florianbracke@users.noreply.github.com>
2023-06-12 11:57:45 +0200

    Remove emtpy href filter (moved to js file)

--- custom-theme/assets/js/stylingScript.js
+++ custom-theme/assets/js/stylingScript.js
@@ -41,22 +41,7 @@ window.addEventListener('load', () => {
 	
 	}
 
-	
-	/* FILTER EMPTY BUTTONS */
 
-	allButtons = document.querySelectorAll('body.logged-in a.wp-block-button__link');
-	allButtons.forEach(button => {
-		if(!button.href){
-			button.classList.add('emptyButton');
-		}
-	});
-
-	let emptyButtonStyles = `.wp-block-button__link.emptyButton{ background-color: rgba(255, 0, 0, 0.493) !important; }`;
-	let styleSheet = document.createElement("style")
-	styleSheet.innerText = emptyButtonStyles
-	document.head.appendChild(styleSheet)
-
-	
 	
 });
 
